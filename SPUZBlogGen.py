54530aa8f30cd06412b6ddb1fc25b50a65e57a5c
649 <649@users.noreply.github.com>
2018-12-27 15:56:33 -0800

    init

--- SPUZBlogGen.py
+++ SPUZBlogGen.py
@@ -12,9 +12,13 @@ print("""
 """)
 
 filename = input("[*] Enter blog post filename: ") + '.html'
-file = open(filename, 'w')
-
-option = True
+if (Path(filename).is_file()):
+    print("[*] ERROR: File already exists!")
+    option = 5
+else:
+    file = open(filename, 'a')
+    stage = 0
+    option = 0
 while(Path(filename).is_file() and option != 5):
     try:
         print("[*] ----- STAGE ONE -----")
@@ -28,6 +32,45 @@ while(Path(filename).is_file() and option != 5):
         if not (1 <= option <= 5):
             raise ValueError()
     except ValueError:
-        print("[*] Error: Invalid Option %d! (1-5)" % option)
+        print("[*] WARNING: Invalid Option %d! (1-5)" % option)
     else:
-        print("[*] Processing. . .")
+        
+
+
+        if(stage == 5):
+            init1 = """
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!----Google Analytics---->	
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115535331-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-115535331-1');
+</script>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-8088146909281544",
+          enable_page_level_ads: true
+     });
+</script>
+<!------------------------>
+
+	<link rel="icon" type="image/png" href="./img/main/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="./img/main/favicon-16x16.png" sizes="16x16" />
+	
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SPUZ : 
+        """
+            file.write(init1)
